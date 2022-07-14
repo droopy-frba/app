f9bbce852f30e404eecfba6ed860b8b7e1d34819
Leonardo Guerberg <leonardo.guerberg@wearepsh.com>
2022-07-14 00:16:54 -0300

    added styled components, fonts, prettier, linter and login component

--- App.tsx
+++ App.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 
+import lightTheme from '@assets/theme/light';
 
 import useCachedResources from './src/hooks/useCachedResources';
 import Navigation from './src/navigation';
-import lightTheme from '@assets/theme/light';
 
 export default () => {
   const isLoadingComplete =
